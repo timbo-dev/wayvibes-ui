ae731b92631c7e40f65e59127d08416301b8796e
Nathan Rocha <dev@timbo.me>
2026-02-02 22:43:20 -0300

    feat: create services module for application services
    - Introduced a new services module to organize and manage application services, including config, sound pack, and Wayvibes services.
    - Improved code structure and maintainability by modularizing service-related functionalities.

---
+++ src-tauri/src/services.rs
@@ -0,0 +1,6 @@
+#[path = "services/config-service.rs"]
+pub mod config_service;
+#[path = "services/sound-pack-service.rs"]
+pub mod sound_pack_service;
+#[path = "services/wayvibes-service.rs"]
+pub mod wayvibes_service;
